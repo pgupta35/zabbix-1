f7d07f9d6e357556d4be5671a2fef291ebabd5a6
Matvey Marinin <marininmatvey@gmail.com>
2013-12-20 11:17:45 +0400

    FIX: svc_status.sh - check for ssh errors

--- svc_status.sh
+++ svc_status.sh
@@ -4,17 +4,29 @@
 #
 # 2013 Matvey Marinin
 #
-# Returns number of errors or empty string
-# 
+# Returns number of Storwize alerts or an empty string
+#
 # Parameters:
 #   $1 = Storwize DNS name/IP
 #
-# Script must be run as zabbix user:
+# Usage:
+#   create external check item with key svc_status.sh["{HOST.CONN}"]
+#
+# For testing purposes script must be run as zabbix user:
 #   sudo -u zabbix /etc/zabbix/externalscripts/svc_status.sh dev-svc1
 #
 # Key-based SSH access to Storwize CLI needs to be configured for zabbix user (see /var/lib/zabbix/.ssh)
 #
-set -e
-
 SVC_STATUS_AWK=/etc/zabbix/externalscripts/svc_status.awk
-ssh -q -o PasswordAuthentication=no $1 lseventlog -expired no -fixed no -monitoring no -message no -order severity | awk -f "$SVC_STATUS_AWK" | wc -l
+
+# run lseventlog command with ssh
+EVENTS=$(ssh -q -o PasswordAuthentication=no $1 lseventlog -expired no -fixed no -monitoring no -message no -order severity)
+SSH_RES=$?
+
+# check for ssh error
+[[ "$SSH_RES" != 0  ]] && exit "$SSH_RES"
+
+# parse event log
+echo "$EVENTS" | awk -f "$SVC_STATUS_AWK" | wc -l
+
+
