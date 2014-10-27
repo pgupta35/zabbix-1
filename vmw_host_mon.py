5c1c796d7a110da83d807bd8ba0a0e55c4d992d8
Matvey Marinin <marininmatvey@gmail.com>
2014-10-27 17:58:25 +0300

    FIX: vmw_host_mon.py - fixed 1-hour difference bug after MSK timezone 2014 change

--- vmw_host_mon.py
+++ vmw_host_mon.py
@@ -12,6 +12,15 @@ from pysphere import VIServer
 from pysphere.resources import VimService_services as VI
 from pysphere.ZSI import FaultException as ZSI_FaultException
 
+#### Calculate actual UTC offset to workaround 'MSK' timezone DST change in 2014 ####
+#### pysphere uses time.timezone to convert datetime from UTC to local timezone  ####
+t = time.time()
+td = (datetime.datetime.utcfromtimestamp(t) - datetime.datetime.fromtimestamp(t))
+utc_offset = (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+time.timezone = utc_offset
+print >> sys.stderr, 'DST offset: ', str(utc_offset)
+####
+
 def usage():
   print >> sys.stderr, "Usage: vmw_host_mon.py <vcenter_zabbix_host> <vcenter_dns_name> <vc_user> <vc_password> <discovery|time|status>"
 
@@ -53,27 +62,32 @@ try:
     #vcenter vmware.host.timediff[host-1249] 1373876039 -1
         
     for x in server._get_object_properties_bulk( hosts.keys(), {'HostSystem':['configManager.dateTimeSystem']} ) :
-      host_id = x.Obj
-      host_name = hosts[x.Obj]
-      hostDateTimeSystem = x.PropSet[0].Val
-      
       try:
+        host_id = x.Obj
+        host_name = hosts[x.Obj]
+        hostDateTimeSystem = x.PropSet[0].Val #can throw AttributeError
+
         ''' call vSphere SDK HostDateTimeSystem.QueryDateTime() method '''
         dtReqClass = VI.QueryDateTimeRequestMsg()
         dtReq = dtReqClass.new__this(hostDateTimeSystem)
         dtReq.set_attribute_type(hostDateTimeSystem.get_attribute_type())
         dtReqClass.set_element__this(dtReq)
-      
+
         timestamp = int(time.time()) #save local time at SDK call to compare with remote host
-        res = server._proxy.QueryDateTime(dtReqClass)._returnval #get remote ESXi host time as time.struct_time tuple
+        res2 = server._proxy.QueryDateTime(dtReqClass)
+        res = res2._returnval #get remote ESXi host time as time.struct_time tuple in a local timezone
+
         host_time = int(time.mktime(res))
         time_diff = host_time - timestamp #time difference in seconds between ESXi host and system running this script
 
         #<hostname> <key> <timestamp> <value>
         print '%s vmware.host.timediff[%s] %d %s' % (vcenter_host, host_id, timestamp, time_diff)
         
-      except ZSI_FaultException as e: ##Exceptions raise for unavailable hosts, skip to prevent log trashing
+      except ZSI_FaultException as e: #this exception is raised for unavailable hosts, hide it to prevent log trashing
         pass
+      except AttributeError as e:
+        print >> sys.stderr, "Host %s: AttributeError %s" % (host_name, e) #if host has no time data, skip it
+	pass
       except Exception as e:
         print >> sys.stderr, "Exception %s.%s: %s" % ( type(e).__module__, type(e).__name__, e )
         pass
@@ -85,7 +99,7 @@ try:
     # host.summary.overallStatus = { gray | green | red | yellow } (ManagedEntityStatus)
     # host.runtimeinfo.connectionState = { connected | disconnected | notResponding } (HostSystemConnectionState)
     # host.runtimeinfo.dasHostState.state = { connectedToMaster | election | fdmUnreachable | hostDown | initializationError | master | networkIsolated | networkPartitionedFromMaster | uninitializationError | uninitialized } (ClusterDasFdmAvailabilityState)
-    # host.runtimeinfo.inMaintenanceMode = { True | False }
+    # host.runtimeinfo.inMaintenanceMode = { true | false }
 
     hostItems = {
       'summary.overallStatus':'status',
